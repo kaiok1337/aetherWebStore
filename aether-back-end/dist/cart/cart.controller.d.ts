488a9b9378dd17319ca3185033f892d73fb9902e
kaiok1337 <kaioklesson@gmail.com>
2023-01-20 18:50:00 -0700

    auto

---
+++ aether-back-end/dist/cart/cart.controller.d.ts
@@ -0,0 +1,12 @@
+import { CartService } from './cart.service';
+import { ItemDTO } from './dto/item.dto';
+export declare class CartController {
+    private cartService;
+    constructor(cartService: CartService);
+    addItemToCart(req: any, itemDTO: ItemDTO): Promise<import("./schemas/cart.schema").Cart>;
+    removeItemFromCart(req: any, { productId }: {
+        productId: any;
+    }): Promise<any>;
+    deleteCart(userId: string): Promise<import("./schemas/cart.schema").Cart>;
+    viewCart(userId: string): Promise<import("./schemas/cart.schema").CartDocument>;
+}
---
+++ aether-back-end/dist/products/dto/create-product.dto.d.ts
@@ -0,0 +1,9 @@
+export declare class CreateProductDto {
+    name: string;
+    price: number;
+    smallQuantity: number;
+    medQuantity: number;
+    lrgQuantity: number;
+    imgUrl: string;
+    type: string;
+}
